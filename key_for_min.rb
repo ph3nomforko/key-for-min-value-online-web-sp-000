69a175c6c2f2100e59fcea41f7bfcc5d05ff2a4c
Codi Kraus <codi.kraus@gmail.com>
2021-05-03 15:19:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_hash.each do |item, value|
+    check_num = value
+    save_value = 0
 
 end
